a11b1f2e2b08f717b78fde1d70eaca127e4e943d
AlloVince <allo.vince@gmail.com>
2016-10-20 16:33:25 +0800

    add polyfill

--- examples/app.js
+++ examples/app.js
@@ -1,14 +1,17 @@
-const ZmxyClient = require('../lib');
+const fs = require('fs');
+const ZmxyClient = require('..').default;
 
 const zmxy = new ZmxyClient({
-    platform: 'bmqb',  //接入商户名
-    appId: '123456',   //芝麻应用App ID
-    appPrivateKey: fs.readFileSync(`${__dirname}/app_private_key.pem`),  //App私钥
-    zmxyPublicKey: fs.readFileSync(`${__dirname}/zmxy_public_key.pem`)   //芝麻公钥
+  platform: 'bmqb',  //接入商户名
+  appId: '123456',   //芝麻应用App ID
+  appPrivateKey: fs.readFileSync(`${__dirname}/app_private_key.pem`),  //App私钥
+  zmxyPublicKey: fs.readFileSync(`${__dirname}/zmxy_public_key.pem`)   //芝麻公钥
 });
 zmxy.verifyIvs({
-    name: '张三',
-    mobile: '12345678901'
+  name: '张三',
+  mobile: '12345678901'
 }).then((result) => {
-    console.log(result);
+  console.log(result);
+}).catch((err) => {
+  console.error(err);
 });
